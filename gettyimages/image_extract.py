48cc56f6cd432ddcc96234e6a91887763a9269da
SonK4m3 <nguyenhongsont1lhp@gmail.com>
2022-07-18 10:46:38 +0700

    Update: readjson, crawl all image

--- gettyimages/image_extract.py
+++ gettyimages/image_extract.py
@@ -1,3 +1,6 @@
+from click import format_filename
+
+
 try:
     from bs4 import BeautifulSoup
     import requests
@@ -20,54 +23,51 @@ BASE_DIR = './gettyimage'
 
 IMAGE_LIST = 'https://www.gettyimages.com/photos/oscars?assettype=image&family=editorial&phrase=oscars&sort=mostpopular&page={}'
 IMAGE_SAVE_DIR = BASE_DIR + '/image_data'
+IMAGE_SAVE_FILE = 'image_data'
 
 def make_dir(directory):
     if not os.path.exists(directory):
         os.makedirs(directory)
 
-
-
-def image_extract(url=IMAGE_LIST, save_dir=IMAGE_SAVE_DIR, start=1):
+def image_extract(url=IMAGE_LIST, save_dir=IMAGE_SAVE_DIR, save_file = IMAGE_SAVE_FILE, start=1):
 
     make_dir(save_dir)
 
     url = url.format(start)
     soup = request_url(url)
 
-    elements = soup.select('div.MosaicAsset-module__galleryMosaicAsset____wxTl article ')
+    elements = soup.select('div.MosaicAsset-module__galleryMosaicAsset____wxTl article')
 
     data = []
     for ele in elements:
         try:
             link = ele.select('a figure picture img')[0]
             img = link['src']
-            
-            # print(href)
-            # print('-----\n\n')
  
             ele_data = {
                 'url' : img,
                 'page' : start
             }
             
-            print(ele_data)
-
             data.append(ele_data)
         except Exception as e:
             print("Error: ", e)
             traceback.print_exc()
 
-    filename = 'image_page{}.json'.format(start)
+    #write list image page
+    filename = save_file + '_page_{}.json'.format(start)
     write_json(save_dir + '/' + filename, data)
+
+    #write list all image        
+    filename = save_file + '.json'
+    rewrite_json(save_dir + '/' + filename, data)
 
 def write_json(filename, data):
     with open(filename, 'w', encoding='utf8') as f:
         json.dump(data, f, indent=4, ensure_ascii=False)
 
-
-def get_image_list(page_number = 1):
+def read_json(filename):
     try:
-        filename = IMAGE_SAVE_DIR + '/' + 'image_page{}.json'.format(page_number)
         with open(filename, 'r') as f:
             data = json.load(f)
             return data
@@ -76,6 +76,20 @@ def get_image_list(page_number = 1):
         traceback.print_exc()
         return []
 
+def rewrite_json(filename, data):
+    new_data = read_json(filename)
+    new_data = new_data + data
+    write_json(filename,data)
+
+def get_all_image():
+    filename = IMAGE_SAVE_DIR + '/' + IMAGE_SAVE_FILE + '.json'
+    data = read_json(filename)
+    return data
+
+def get_image_list(page_number = 1):
+    filename = IMAGE_SAVE_DIR + '/' + IMAGE_SAVE_FILE + '_page_{}.json'.format(page_number)
+    data = read_json(filename)
+    return data
 
 def req_url(url):
     session = requests.Session()
@@ -104,6 +118,9 @@ def request_url(url):
     soup = BeautifulSoup(response.text, 'html.parser')
     return soup
 
+def image_crawler(url=IMAGE_LIST, save_dir=IMAGE_SAVE_DIR, save_file=IMAGE_SAVE_FILE, start=1, number=5):
+    for i in range(start, start + number + 1):
+        image_extract(url,save_dir,save_file,i)
+
 if __name__ == '__main__':
-    for i in range(1, 6): 
-        image_extract(start=i)
+    image_crawler()
