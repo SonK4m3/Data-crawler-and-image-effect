c0b0e9a342b4f2b909e787cb5dc0e4f4fdda1e45
SonK4m3 <nguyenhongsont1lhp@gmail.com>
2022-08-10 09:56:52 +0700

    Fix: get face mesh and stick flag into face

--- face_mesh_detection_2/face_mesh_detection.py
+++ face_mesh_detection_2/face_mesh_detection.py
@@ -1,24 +1,32 @@
-import cv2 as cv
-import numpy as np
-import mediapipe as mp
+try:
+    import cv2 as cv
+    import numpy as np
+    import mediapipe as mp
+    import os
+    import argparse
+except Exception as e:
+    print('Caught error while importing: {}'.format(e))
 
-PATH = './Photos/man.jpg'
+IMAGE_PATH = './Photos/DSC_6730.JPG'
+FLAG_PATH = './Photos/vietnam_flag.jpg'
 SAVE_DIR = './Save image'
+
+def make_dir(directory):
+    if not os.path.exists(directory):
+        os.makedirs(directory)
 
 def resized_image(image, scale = 500):
     height, width = image.shape[:2]
     x = scale / width
     return cv.resize(image, (scale, int(height * x)))
 
-
-def get_face_detection(path=PATH):
+def get_face_detection(image_path):
     mp_face_detection = mp.solutions.face_detection
-    mp_drawing = mp.solutions.drawing_utils
     
     location_face_detection = []
     
     with mp_face_detection.FaceDetection(model_selection = 1, min_detection_confidence = 0.1) as face_detection:
-        image = cv.imread(PATH)
+        image = cv.imread(image_path)
         height, width = image.shape[:2]
         
         results = face_detection.process(cv.cvtColor(image, cv.COLOR_BGR2RGB))
@@ -36,21 +44,29 @@ def get_face_detection(path=PATH):
 
         return location_face_detection
   
-def get_face_mesh(path=PATH, xmin=0, ymin=0, width=0, height=0):
+def get_face_mesh(image_path, xmin, ymin, width, height, flag_path, save_image):
+        
+    #import model face mesh
     mp_face_mesh = mp.solutions.face_mesh
-    mp_drawing = mp.solutions.drawing_utils
-    mp_drawing_styles = mp.solutions.drawing_styles
     
     with mp_face_mesh.FaceMesh(
         static_image_mode = True, 
         max_num_faces = 3, 
         refine_landmarks = True, 
         min_detection_confidence = 0.5) as face_mesh:
-        image = cv.imread(PATH)
+        image = cv.imread(image_path)
         image = image[ymin:ymin+height, xmin:xmin+width]
         
-        mask = np.zeros((image.shape[0], image.shape[1]), dtype='uint8')
-
+        flag = cv.imread(flag_path)
+        flag = cv.resize(flag, (image.shape[1], image.shape[0]))
+        
+        mask = np.zeros(flag.shape[:2], dtype='uint8')
+        
+        face_mask = np.zeros(flag.shape[:2], dtype='uint8')
+        right_eye_mask = np.zeros(flag.shape[:2], dtype='uint8')
+        left_eye_mask = np.zeros(flag.shape[:2], dtype='uint8')
+        lips_mask = np.zeros(flag.shape[:2], dtype='uint8')
+        
         results = face_mesh.process(cv.cvtColor(image, cv.COLOR_BGR2RGB))
         
         if not results.multi_face_landmarks:
@@ -58,34 +74,131 @@ def get_face_mesh(path=PATH, xmin=0, ymin=0, width=0, height=0):
         
         annotated_image = image.copy()
         for face_landmarks in results.multi_face_landmarks:
-            landmark_points = []
-            
-            for lm in face_landmarks.landmark:
-                landmark_points.append((lm.x * width, lm.y * height))  
-            # print(face_landmarks)
-            # mp_drawing.draw_landmarks(
-            #     annotated_image,
-            #     face_landmarks,
-            #     mp_face_mesh.FACEMESH_TESSELATION,
-            #     None,
-            #     mp_drawing_styles.get_default_face_mesh_tesselation_style()
-            # )
+            #get face landmark point 
+            face_landmark_points = get_landmark_points(
+                                image=annotated_image,
+                                face_landmarks=face_landmarks, 
+                                mask=mp_face_mesh.FACEMESH_FACE_OVAL)
 
-            points = np.array(landmark_points, np.int32)
-            convexhull = cv.convexHull(points)
-            
-            # cv.polylines(annotated_image, [convexhull], True, (0,0,255), thickness=3)
-            cv.fillConvexPoly(mask, convexhull, 255)
+            face_points = np.array(face_landmark_points, np.int32)
+            face_convexhull = cv.convexHull(face_points)
+            cv.fillConvexPoly(face_mask, face_convexhull, 255)
 
-            cropped_face = cv.bitwise_and(annotated_image, annotated_image, mask=mask)
-        cv.imshow('a', resized_image(cropped_face))
-        cv.waitKey(0)
+            # eyes
+            right_eye_landmark_points = get_landmark_points(
+                                image=annotated_image,
+                                face_landmarks=face_landmarks, 
+                                mask=mp_face_mesh.FACEMESH_RIGHT_EYE)
+
+            right_eye_points = np.array(right_eye_landmark_points, np.int32)
+            right_eye_convexhull = cv.convexHull(right_eye_points)
+            cv.fillConvexPoly(right_eye_mask, right_eye_convexhull, 255)
+            
+            left_eye_landmark_points = get_landmark_points(
+                                image=annotated_image,
+                                face_landmarks=face_landmarks, 
+                                mask=mp_face_mesh.FACEMESH_LEFT_EYE)
+
+            left_eye_points = np.array(left_eye_landmark_points, np.int32)
+            left_eye_convexhull = cv.convexHull(left_eye_points)
+            cv.fillConvexPoly(left_eye_mask, left_eye_convexhull, 255)
+            
+            #lips
+            lips_landmark_points = get_landmark_points(
+                                image=annotated_image,
+                                face_landmarks=face_landmarks, 
+                                mask=mp_face_mesh.FACEMESH_LIPS)
+
+            lips_points = np.array(lips_landmark_points, np.int32)
+            lips_convexhull = cv.convexHull(lips_points)
+            cv.fillConvexPoly(lips_mask, lips_convexhull, 255)
+            #mask
+            mask = cv.bitwise_or(mask, face_mask)
+            mask = cv.bitwise_xor(mask, right_eye_mask)
+            mask = cv.bitwise_xor(mask, left_eye_mask)
+            mask = cv.bitwise_xor(mask, lips_mask)
+            #next step
+            stick_flag_into_face(image, mask, flag, save_image) 
+
+def stick_flag_into_face(image, mask, flag, save_image):
+    #only face mask
+    only_face_mask = cv.bitwise_and(image, image, mask=mask)
+            
+    #no face
+    img_head_mask = cv.bitwise_not(mask)
+    img_noface = cv.bitwise_and(image, image, mask=img_head_mask)
+    
+    #face flag
+    flag_img = np.zeros(image.shape, np.uint8)
+    flag_img = cv.bitwise_and(flag, flag, mask=mask)
+    
+    #blend
+    alpha = 0.5
+    beta = 1 - alpha
+    new_face = cv.addWeighted(only_face_mask, alpha, flag_img, beta, 0.0)
+    
+    #add 
+    result = cv.add(img_noface, new_face)
+    result2 = cv.add(img_noface, flag_img)
+    
+    monoMaskImage = cv.split(mask)[0] # reducing the mask to a monochrome
+    br = cv.boundingRect(monoMaskImage) # bounding rect (x,y,width,height)
+    centerOfBR = (br[0] + br[2] // 2, br[1] + br[3] // 2)
+
+    final_result = cv.seamlessClone(result2, result, mask, centerOfBR, cv.MIXED_CLONE)
+    
+    #save image
+    name = save_image + '/' + 'only_face_mask' + '.jpg'
+    cv.imwrite(name, only_face_mask)
+    name = save_image + '/' + 'flag_img' + '.jpg'
+    cv.imwrite(name, flag_img)
+    name = save_image + '/' + 'new_face' + '.jpg'
+    cv.imwrite(name, new_face)
+    name = save_image + '/' + 'img_noface' + '.jpg'
+    cv.imwrite(name, img_noface)
+    name = save_image + '/' + 'result' + '.jpg'
+    cv.imwrite(name, result)
+    name = save_image + '/' + 'result2' + '.jpg'
+    cv.imwrite(name, result2)
+    name = save_image + '/' + 'final_result' + '.jpg'
+    cv.imwrite(name, final_result)
+
+    #show image
+    cv.imshow('only', only_face_mask)
+    cv.imshow('flag', flag_img)
+    cv.imshow('new', new_face)
+    cv.imshow('no', img_noface)
+    cv.imshow('final', result)
+    cv.imshow('final 2', result2)
+    cv.imshow('final s', final_result)
+
+    cv.waitKey(0)
+
+def get_landmark_points(image, face_landmarks, mask):
+    list_x, list_y = zip(*mask)
+    list_mask = list(set(list_x + list_y))
+    height, width = image.shape[:2]
+    
+    landmark_points = list([(face_landmarks.landmark[ind].x * width, face_landmarks.landmark[ind].y * height) for ind in list_mask])
+    return landmark_points
 
 if __name__ == '__main__':
-    face_detections = get_face_detection()
-    print(face_detections)
-    for face_detection in face_detections:
-        x, y, w, h = face_detection[:4]
-        print("{} {} {} {}".format(x, y, w, h))
-        get_face_mesh(xmin=x, ymin=y, width=w, height=h)
+    parser = argparse.ArgumentParser(description='Stick flag to face')
+    parser.add_argument('-src', '--source-image', help="face is sticked", default=IMAGE_PATH, type=str)
+    parser.add_argument('-flag', help="image to stick into face", default=FLAG_PATH, type=str)
+    parser.add_argument('--sav', help='save dir', default=SAVE_DIR, type=str)
+    args = parser.parse_args()
     
+    face_detections = get_face_detection(args.source_image)
+    #debug
+    # print(face_detections)
+    
+    make_dir(args.sav)
+    
+    for num, face_detection in enumerate(face_detections):
+        x, y, w, h = face_detection
+        #debug
+        # print("{} {} {} {}".format(x, y, w, h))
+        save_image = args.sav + '/' + 'image_' + str(num)
+        make_dir(save_image)
+        get_face_mesh(args.source_image, x, y, w, h, args.flag, save_image)
